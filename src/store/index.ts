9a3e0cb349abdd05aadedf4167cd5354f624abac
Gabriel <gabriel.pires@a3data.com.br>
2023-08-26 15:46:36 -0300

    dispatch actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,22 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { useSelector, TypedUseSelectorHook } from 'react-redux'
+
+//ao dar um console.log na action temos a seguinte estrutura:
+//  { type: 'todo/add', payload : 'Qualquer coisa' }
+// o type é: 'reducer /action do reducer' 
+// o playload é o conteúdo que eu passo no disparo (dispatch) na action
+
 
 const todoSlice = createSlice({ // a slice is a chunk of a state
   name: 'todo',
   initialState: ['Fazer café', 'estudar redux'],
 
-  reducers: {}
+  reducers: {
+    add: (state, action) => {
+      state.push(action.payload.newTodo)
+    }
+
+  }
 })
 
 export const store = configureStore({
@@ -12,3 +24,8 @@ export const store = configureStore({
     todo: todoSlice.reducer,
   }
 })
+
+export const { add } = todoSlice.actions
+
+export type RootState = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector

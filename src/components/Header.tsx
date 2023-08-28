679ba48bb37b72293d6799e275bfd33a8a6080a5
Gabriel <gabriel.pires@a3data.com.br>
2023-08-28 12:40:19 -0300

    highlight current lesson

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
+import { useAppSelector } from "../store"
+
 export function Header () {
+
+  const { currentModule, currentLesson } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    const currentLesson =
+      state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+
+    const currentModule = state.player.course.modules[currentModuleIndex]
+
+    return { currentModule, currentLesson }
+  })
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold" >Redux Foundation</h1>
-      <span className="text-sm text-zinc-400">Initializing Redux</span>
+      <h1 className="text-2xl font-bold" >{currentLesson.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
     </div>
   )
 }
